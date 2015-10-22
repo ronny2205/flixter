1d4333060703ec78423fcb58145bdcf6e177c3e9
Ronny <ronny2205@gmail.com>
2015-10-22 18:14:15 +0000

    adding student enrollment

---
+++ test/controllers/enrollments_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class EnrollmentsControllerTest < ActionController::TestCase
+  test "create enrollment" do
+  	course = FactoryGirl.create(:course)
+  	u1 = FactoryGirl.create(:user)
+    sign_in u1
+   
+   assert_difference 'Enrollment.count' do
+    	post :create, :course_id => course.id, :user_id => u1.id, :enrollment =>
+    	{}
+    end
+    assert_redirected_to course_path(course)
+
+  end
+end
